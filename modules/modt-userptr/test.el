32dfdd67dd5b324b19d5828dca4b25b43556e69a
Aurelien Aptel <aaptel@suse.com>
2015-10-21 19:48:37 +0200

    add user-ptr test

---
+++ modules/modt-userptr/test.el
@@ -0,0 +1,19 @@
+
+(require 'ert)
+(require 'module-test-common)
+
+;; #$ works when loading, buffer-file-name when evaluating from emacs
+(module-load (module-path (or #$ (expand-file-name (buffer-file-name)))))
+
+(ert-deftest modt-userptr-fun-test ()
+  (let* ((n 42)
+         (v (modt-userptr-make n))
+         (r (modt-userptr-get v)))
+    
+    (should (eq (type-of v) 'user-ptr))
+    (should (integerp r))
+    (should (= r n))))
+
+;; TODO: try to test finalizer
+
+
