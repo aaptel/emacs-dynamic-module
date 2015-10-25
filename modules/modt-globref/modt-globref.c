59c50a2748f814471177abeba80e2715e682f6d0
Philipp Stephani <phst@google.com>
2015-10-25 22:12:57 +0100

    Fix arity in test

--- modules/modt-globref/modt-globref.c
+++ modules/modt-globref/modt-globref.c
@@ -47,7 +47,7 @@ int emacs_module_init (struct emacs_runtime *ert)
   emacs_env *env = ert->get_environment (ert);
   Qnil = env->intern (env, "nil");
   Qt = env->intern (env, "t");
-  bind_function (env, "modt-globref-make", env->make_function (env, 1, 1, Fmodt_globref_make, NULL));
+  bind_function (env, "modt-globref-make", env->make_function (env, 0, 0, Fmodt_globref_make, NULL));
   provide (env, "modt-globref");
   return 0;
 }
