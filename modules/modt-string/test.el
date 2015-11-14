1a175c9564a75eafbc3522a27c12213466ea94a6
Aurélien Aptel <aurelien.aptel@gmail.com>
2015-11-14 17:38:31 +0100

    merge all tests into one

--- modules/modt-string/test.el
+++
@@ -1,9 +0,0 @@
-
-(require 'ert)
-(require 'module-test-common)
-
-;; #$ works when loading, buffer-file-name when evaluating from emacs
-(module-load (module-path (or #$ (expand-file-name (buffer-file-name)))))
-
-(ert-deftest modt-string-a-to-b-test ()
-  (should (string= (modt-string-a-to-b "aaa") "bbb")))
